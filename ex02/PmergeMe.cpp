d70764263e489f8bf0127f2d878e019925115260
Harivonjy Princys Andrianiaina <hariandr@c1r8s7.42antananarivo.mg>
2025-12-31 11:55:33 +0300

    tay

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -1,1 +1,52 @@
 #include <PmergeMe.hpp>
+
+static bool	isNumber(std::string const & str)
+{
+	size_t	i = 0;
+	
+	if (str.empty() == true)
+		return (false);
+
+	if (str[0] == '+')
+		i++;
+
+	for (; i < str.length() && isdigit(str[i]) == true; i++);
+
+	return (i == str.length());
+}
+
+PmergeMe::PmergeMe(){}
+
+PmergeMe::~PmergeMe(){}
+
+PmergeMe::PmergeMe(PmergeMe const & copy)
+{
+	*this = copy;
+}
+
+PmergeMe & PmergeMe::operator=(PmergeMe const & copy)
+{
+	if (this != &copy)
+		this->_data = copy._data;
+
+	return (*this);
+}
+
+PmergeMe::PmergeMe(std::string const & input)
+{
+	std::istringstream	ss(input);
+	std::string			token;
+
+	while (ss >> token)
+	{
+		if (isNumber(token) == false)
+			throw std::invalid_argument("Error: " + token + "is not a positive Integer");
+
+		long	num = std::strtoll(token.c_str(), NULL, 10);
+		
+		if (num > INT_MAX)
+			throw std::out_of_range("Error: " + token + " is out of range");
+
+		_data.push_back(static_cast<int>(num));
+	}
+}
--- ex02/header/PmergeMe.hpp
+++ ex02/header/PmergeMe.hpp
@@ -1,18 +1,25 @@
 #pragma once
 
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <climits>
+#include <cctype>
 #include <vector>
 #include <deque>
 
 class	PmergeMe
 {
     public:
-							PmergeMe();
+							PmergeMe(std::string const &);
+
 							~PmergeMe();
-							PmergeMe(PmergeMe const & copy);
-		PmergeMe &			operator=(PmergeMe const & copy);
 
 	private:
 		std::vector<int>	_data;
+
+							PmergeMe();
+							PmergeMe(PmergeMe const &);
+		PmergeMe &			operator=(PmergeMe const &);
 
 };
